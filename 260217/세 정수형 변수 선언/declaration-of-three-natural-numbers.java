616de20300a7c65535133beb08d049d3393e82cb
leelaal02 <168619290+leelaal02@users.noreply.github.com>
2026-02-17 20:18:32 +0900

    Title: 세 정수형 변수 선언, Time: 98ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260217/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 23;
+        int c = 30;
+        //System.out.print(a + " + " + b  + " = " + c);
+
+        System.out.printf("%d + %d = %d", a, b, c);
+        // Please write your code here.
+    }
+}
